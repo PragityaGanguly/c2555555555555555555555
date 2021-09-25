7edf2bd91c241e07cf141665925e27232b815e02
PragityaGanguly <79367649+PragityaGanguly@users.noreply.github.com>
2021-09-25 19:07:29 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,17 @@
+var option={
+    isStatic:false,
+    restitution=0.3,
+    friction:0
+    density=1.2
+    Image()
+
+
+    function keyPressed{} {
+
+        if keyCode === UP_ARROW) {
+
+Matter.body.applyforce (paperObject.body,paperObject.body.position,(x:130,y:=145)):
+
+        }
+
+    }
